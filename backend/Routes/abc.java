9be76bc06f8b5a6599f22def3069d86ec2048534
Srinivas <vasus4990@gmail.com>
2024-11-30 21:53:50 +0530

    pushed pulled sources

--- backend/Routes/abc.java
+++ backend/Routes/abc.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class abc{
     public static void main(String[] args) {
-        HashSet<Integer> a = new HashSet<Integer>();
-       a.add
+        HashMap<Character, Integer> freq = new HashMap<>();
+        freq.put('c', freq.getOrDefault(freq, 0));
+        
     }
 }
