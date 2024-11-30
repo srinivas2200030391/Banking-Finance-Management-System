54a40a59e2f8613fda2c0e6fefb339e978b9bdc1
Srinivas <vasus4990@gmail.com>
2024-11-30 22:02:55 +0530

    back end url updated

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,6 +1,8 @@
 const config = {
-  baseURL: "https://mswdprojects14.onrender.com",
+  //baseURL: "https://mswdprojects14.onrender.com",
   //baseURL: "http://localhost:2014",
+  baseUrl:
+    "https://banking-finance-management-system-jwtm-8yj03p2ug.vercel.app/?vercelToolbarCode=7g_3R5SrQJy26hw/",
 };
 
 export default config;
